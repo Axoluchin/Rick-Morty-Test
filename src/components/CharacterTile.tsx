95062e2b95894c1f7fc9454a07d260c0043e3b57
Damian Huchin <huchin.h.damian@gmail.com>
2023-03-01 08:41:09 -0600

    Better character card

--- src/components/CharacterTile.tsx
+++ src/components/CharacterTile.tsx
@@ -1,20 +1,52 @@
-import Col from "react-bootstrap/Col";
-import { Character } from "../providers/api/models";
+import { useState, useEffect } from 'react'
+import Col from 'react-bootstrap/Col'
+import axios from 'axios'
+
+import { Character, Episode } from '../providers/api/models'
 
 interface CharacterTileProps {
-    character: Character
+  character: Character
 }
 
-export function CharacterTile({character}:CharacterTileProps) {
-    return (
-        <Col md="6" className="character-tile" key={character.id}>
-            <div className="container">
-                <img src={character.image}></img>
-                <div className="info">
-                    <p>Id: <small>{character.id}</small></p>
-                    <p>Name: <small>{character.name}</small></p>
-                </div>
-            </div>
-        </Col>
-    )
+export function CharacterTile({ character }: CharacterTileProps) {
+  const [Episode, setEpisode] = useState<string | undefined>()
+
+  useEffect(() => {
+    axios
+      .get<Episode>(character.episode[0])
+      .then(({ data }) => setEpisode(data.name))
+      .catch(() => setEpisode('Not Found'))
+  }, [character])
+
+  const setColor = {
+    Alive: '#75c850',
+    Dead: 'red',
+    unknown: 'yellow',
+  }
+  return (
+    <Col md="6" className="character-tile" key={character.id}>
+      <div className="character-container">
+        <img src={character.image} />
+        <div className="info">
+          <h4>{character.name}</h4>
+          <p>
+            <small>
+              <span style={{ color: setColor[character.status] }}>●</span>{' '}
+              {character.status} - {character.species}
+            </small>
+          </p>
+          <p>
+            <small>Last known location:</small>
+            <br />
+            {character.location.name}
+          </p>
+          <p>
+            <small>First seen in:</small>
+            <br />
+            {Episode?.split('#')[0] || 'Loading...' }
+          </p>
+        </div>
+      </div>
+    </Col>
+  )
 }
--- src/providers/api/models/Character.ts
+++ src/providers/api/models/Character.ts
@@ -1,5 +1,21 @@
+type status = 'Alive' | 'Dead' | 'unknown'
+
+interface source {
+  name: string
+  url: string
+}
+
 export interface Character {
-    id: string
-    name: string
-    image: string
+  id: number
+  name: string
+  status: status
+  species: string
+  type: string
+  gender: string
+  origin: source
+  location: source
+  image: string
+  episode: string[]
+  url: string
+  created: string
 }
